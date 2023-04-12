5ff3784c0fa92d165c06c65fbfbabebb15a35db5
Taha Khaled Saad <116809865+TahaKhaledSaad@users.noreply.github.com>
2023-04-12 14:02:00 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,32 @@
-var allProducts = document.querySelectorAll(".container .row .card")
-var btn = document.querySelector("#showPrice")
-var content = document.querySelector(".content")
-var priceSection = document.querySelector(".price-sec")
+var allProducts = document.querySelectorAll(".container .row .card");
+var btn = document.querySelector("#showPrice");
+var content = document.querySelector(".content");
+var priceSection = document.querySelector(".price-sec");
 var card_title = document.querySelector(".card-title");
+var overlay = document.querySelector(".overlay");
+var overlayCounter = document.querySelector("#counter")
 
 var totalPrice = 0;
-var counter=0;
+var counter = 0;
 
-allProducts.forEach(function (item){
-    item.onclick= function (){
-        totalPrice += parseInt (item.getAttribute("price")) 
-        content.innerHTML += "<small>Product No</small>" + (counter+=1) + ". "+item.textContent + "<br/>";
-        if(content.innerHTML != null){
-            btn.style.display= "block"
-        }
-        console.log(item.card_title)
+allProducts.forEach(function (item) {
+  item.onclick = function () {
+    totalPrice += parseInt(item.getAttribute("price"));
+    content.innerHTML +=
+      "<small>Product No</small>" +
+      (counter += 1) +
+      ". " +
+      item.textContent +
+      "<br/>";
+    if (content.innerHTML != null) {
+      btn.style.display = "block";
     }
-})
+    console.log(item.card_title);
+    overlay.style.display = "flex";
+    overlayCounter.innerHTML = counter;
+  };
+});
 
-btn.onclick =function(){
-    priceSection.innerHTML=totalPrice + "<small> LE</small>"
-}
-
-
+btn.onclick = function () {
+  priceSection.innerHTML = totalPrice + "<small> LE</small>";
+};
